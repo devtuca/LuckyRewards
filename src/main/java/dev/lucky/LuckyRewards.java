cdae6b99f9fd07371abcc92d6ac78f181eed4557
htyns123 <sirzarybr@hotmail.com>
2020-09-26 02:51:27 -0300

    Initial commit

---
+++ src/main/java/dev/lucky/LuckyRewards.java
@@ -0,0 +1,17 @@
+package dev.lucky;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LuckyRewards extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
